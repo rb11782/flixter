7cf20dd83b921f6191f058a1a4774b5cb21eef3f
Raymond Bertrand <rb11782@gmail.com>
2019-09-24 21:07:45 +0000

    All is working at this point except for video format download.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
+  private
 
-   private
+  # def require_authorized_for_current_course
+  #   if current_lesson.section.course != current_user.enrolled_in?
+  # render plain: "Unauthorized", status: :unauthorized
+  #   end
+  # end
+
+  #   flash[:notice] = "You need to be enrolled in the course to see the lessons."
+  #   # redirect_to <%= link_to 'See All Courses', courses_path, class: 'nav-link' %>
+  #   # end
+  # end
+
+  
+  
 
   helper_method :current_lesson
   def current_lesson
