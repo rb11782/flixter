fd23c74287b6402580beaec045492be266bb2ee6
Raymond Bertrand <rb11782@gmail.com>
2019-09-24 21:12:19 +0000

    At this point, the video download format is not working and a new argument error is showing up in the LessonsController show.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,16 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
   private
 
-  # def require_authorized_for_current_course
-  #   if current_lesson.section.course != current_user.enrolled_in?
-  # render plain: "Unauthorized", status: :unauthorized
-  #   end
-  # end
+  def require_authorized_for_current_course
+    if current_lesson.section.course != current_user.enrolled_in?
+  render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   #   flash[:notice] = "You need to be enrolled in the course to see the lessons."
   #   # redirect_to <%= link_to 'See All Courses', courses_path, class: 'nav-link' %>
