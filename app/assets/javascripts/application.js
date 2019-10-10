aebec3e3464c622292b66b99c7cf0e63eeb6df0c
Raymond Bertrand <rb11782@gmail.com>
2019-10-10 00:13:31 +0000

    The modal in working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
 //= require jquery-ui
