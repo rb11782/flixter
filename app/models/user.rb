361e3060cc5c2ca945084f7267f4f246e2a9ccdf
Raymond Bertrand <rb11782@gmail.com>
2019-10-17 21:51:43 +0000

    I have completed my flixter application.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def enrolled_courses
+    return enrollments.collect(&:course)
+  end
+
 
   def enrolled_in?(course)
     enrolled_courses = enrollments.collect(&:course)
